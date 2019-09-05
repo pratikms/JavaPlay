4f81676d7ed96706922b71aeb1e6d5205726295e
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2019-09-05 23:29:30 +0530

    Add standard implementation of Insertion Sort

--- InsertionSort/src/project/java/insertionsort/Main.java
+++ InsertionSort/src/project/java/insertionsort/Main.java
@@ -2,29 +2,21 @@ package project.java.insertionsort;
 
 public class Main {
 
-    public static void swap(int[] array, int i, int j) {
-
-        if (i == j) {
-            return;
-        }
-
-        int temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-
-    }
-
     public static void main(String[] args) {
 
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
 
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex <= intArray.length - 1; firstUnsortedIndex++) {
-            for (int i = firstUnsortedIndex - 1; i >= 0; i--) {
-                if (intArray[i] > intArray[firstUnsortedIndex]) {
-                    swap(intArray, i, firstUnsortedIndex);
-                    firstUnsortedIndex = i;
-                }
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+
+            int newElement = intArray[firstUnsortedIndex];
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
             }
+
+            intArray[i] = newElement;
+
         }
 
         for (int i :
